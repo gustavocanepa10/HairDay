871faea5101a7b91dae61528b69d123ed25fb2e5
gustavocanepa10 <gustavociscotto@gmail.com>
2024-12-26 10:27:57 -0300

    Adicionando babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
-const HTMLWebPackPlugin = require("html-webpack-plugin")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     target:"web",
@@ -20,17 +21,38 @@ module.exports = {
         liveReload: true,
     },
     plugins: [
-        new  HTMLWebPackPlugin({
+        new  HTMLWebpackPlugin({
             template: path.resolve(__dirname,"index.html"),
             favicon: path.resolve("src", "assets/scissors.svg")
             
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: path.resolve(__dirname,"src","assets"),
+                to:path.resolve(__dirname,"dist","src","assets")
+            },
+
+            ]
+               
+            ,
+        }),
     ],
     module: {
         rules: [
             {
                 test:/\.css$/,
                 use: ["style-loader","css-loader"], 
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    }
+                }
             }
         ]
     }
