2674fd73ce289ba79cbac837a5b916f8f668807f
gustavocanepa10 <gustavociscotto@gmail.com>
2024-12-27 10:07:35 -0300

    Configuração finalizada

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 "use strict"
 
+// importação da Configuração do dayjs
+import "./libs/dayjs.js"
+
 // CSS
 import "./styles/global.css"
 import "./styles/schedule.css"
 import "./styles/form.css"
+
+
